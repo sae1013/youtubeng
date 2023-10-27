caa2dbb1b7a2fd21f0b54ebcce388e25ec555a43
MinwooJung <sae1013@gmail.com>
2023-10-27 22:35:58 +0900

    Working on feed page

--- src/pages/home/MainHomePage.tsx
+++ src/pages/home/MainHomePage.tsx
@@ -2,7 +2,16 @@ import React from 'react';
 import styles from './MainHomePage.module.scss';
 
 function MainHomePage() {
-  return <div className={styles['home-container']}></div>;
+  // 브라우저 크기를 계속 읽어서, 렌더링할갯수를 정해야함
+  return (
+    <div className={styles['main-container']}>
+      <section className={styles['main-sidebar']}>sidebar</section>
+      <section className={styles['main-content']}>
+        {/* grid container */}
+        <div></div>
+      </section>
+    </div>
+  );
 }
 
 export default MainHomePage;
