941d768d6303990d56598da4b2c3377dd2f5c22b
MinwooJung <sae1013@gmail.com>
2023-10-28 21:48:25 +0900

    temp

--- src/pages/home/MainHomePage.tsx
+++ src/pages/home/MainHomePage.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import styles from './MainHomePage.module.scss';
+import { videoItems } from '@/data/videoItem';
 
 function MainHomePage() {
-  // 브라우저 크기를 계속 읽어서, 렌더링할갯수를 정해야함
   return (
     <div className={styles['main-container']}>
       <section className={styles['main-sidebar']}>sidebar</section>
       <section className={styles['main-content']}>
-        {/* grid container */}
+        <ul className={styles['grid-container']}>
+          {videoItems.map((video) => {
+            return (
+              <li className={styles['grid-item']}>
+                <div className={styles['item-image-wrapper']}>
+                  <img src={video.thumbnail}></img>
+                </div>
+                <div className={styles['item-content']}>
+                  <h1 className={styles['item-content__title']}>{video.title}</h1>
+                  <div className={styles['item-content__sub-wrapper']}>
+                    <p className={styles['sub-author']}>{video.author}</p>
+                    <p className={styles['sub-created']}>{video.postedAt}</p>
+                  </div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
         <div></div>
       </section>
     </div>
